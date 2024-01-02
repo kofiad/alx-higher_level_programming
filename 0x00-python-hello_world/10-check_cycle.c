80e9caf0cbaab243e9f0610e73fe3eb52230d2d2
kofiad <kofiadaba@gmail.com>
2024-01-02 17:13:08 +0000

    completed advanced tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: a singly linked list
+ * return: 0 if there is no cycle
+ * 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+
+}
