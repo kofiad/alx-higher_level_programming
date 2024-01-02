15a8bae0101ef4e4b9690bb9ae55ad2cf18ab82b
kofiad <kofiadaba@gmail.com>
2024-01-02 20:34:59 +0000

    completed task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,5 +9,21 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *apple, *orange;
 
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	apple = list->next;
+	orange = list->next->next;
+
+	while (apple && orange && orange->next)
+	{
+		if (apple = orange)
+			return (1);
+
+		apple = apple->next;
+		orange = orange->next->next;
+	}
+	return (0);
 }
