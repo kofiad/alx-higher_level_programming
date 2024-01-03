9132612257819a90820f06ea617258786a351d98
kofiad <kofiadaba@gmail.com>
2024-01-03 18:40:24 +0000

    completed task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for num in range(1, 101):
-        if num % 3 == 0:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz".format(num), end=' ')
+        elif num % 3 == 0:
             print("Fizz".format(num), end=' ')
         elif num % 5 == 0:
             print("Buzz".format(num), end=' ')
-        elif num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz".format(num), end=' ')
         else:
             print("{}".format(num), end=' ')
