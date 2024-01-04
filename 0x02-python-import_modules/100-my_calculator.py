4b0cee3aa0159941775cdc618b2b9f2e0b465252
kofiad <kofiadaba@gmail.com>
2024-01-04 22:34:15 +0000

    task 6 complete

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py a operator b")
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     op = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(op.keys()):
