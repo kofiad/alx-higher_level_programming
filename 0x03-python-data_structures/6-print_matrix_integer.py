1979fc31c53749368a6244fa8515c7a98e3e0bf5
kofiad <kofiadaba@gmail.com>
2024-01-07 21:34:40 +0000

    task complete

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for e in range(len(matrix[row])):
             print("{:d}".format(matrix[row][e]), end=" ")
+            if e != (len(matrix[row]) - 1):
+                    print(" ", end="")
         print()
