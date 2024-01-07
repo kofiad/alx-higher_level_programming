97240272209eeb3b788d6870628f72eb4c145b9b
kofiad <kofiadaba@gmail.com>
2024-01-07 20:29:56 +0000

    task complete

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,5 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return (my_list)
-    else:
-        my_list[idx] = element
+    new_list = my_list[:]
+    new_list[idx] = element
+    return (new_list)
+
