e6967e42d15292df1cecadfae3dd5289aafed52a
kofiad <kofiadaba@gmail.com>
2024-01-07 22:13:09 +0000

    task complete

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    div_list = []
+    for i in my_list:
+        multiple_2 = i % 2 == 0
+        div_list.append(multiple_2)
+    return(div_list)
