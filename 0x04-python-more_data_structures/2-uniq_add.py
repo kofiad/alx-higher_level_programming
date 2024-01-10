f2e7a6feb6002fb59be9632d91f7f2f34676eb5e
kofiad <kofiadaba@gmail.com>
2024-01-10 22:58:37 +0000

    task complete

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-
+    unique_set = set()
+    sum_unique = 0
+    for element in my_list:
+        if element not in unique_set:
+            unique_set.add(element)
+            sum_unique += element
+    return sum_unique
