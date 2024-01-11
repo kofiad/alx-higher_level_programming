a4a786cf2cc6c01e05583fdf58e3a71166dc4c24
kofiad <kofiadaba@gmail.com>
2024-01-11 01:04:49 +0000

    task complete

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    for key, value in a_dictionary.items():
+        updated_value = value * 2
+        new_dictionary[key] = updated_value
+        return new_dictionary
