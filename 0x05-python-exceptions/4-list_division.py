58f2af6da1bea55dd8d3686c61641023983cd3b2
Kofiad <kofiadaba@gmail.com>
2024-01-22 21:46:39 +0000

    task complete

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = [0] * list_length
+    new_list = []
     for i in range(list_length):
         try:
             result[i] = my_list_1[i] / my_list_2[i]
@@ -11,5 +11,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            pass
-    return result
+            new_list.append(result)
+    return (new_list)
