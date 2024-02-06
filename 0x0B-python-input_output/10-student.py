b440b516d927997fd939702253a5ba50d4275f74
kofiad <kofiadaba@gmail.com>
2024-02-06 18:11:00 +0000

    task complete

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a student class"""
+
+
+class Student:
+    """Defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation of the student class
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionary representation of a Student instance
+
+        Returns:
+            dictionary: representation of a Student instance
+        """
+        if (isinstance(attrs, list) and
+            all(isinstance(element, str) for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
