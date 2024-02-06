378ca8fe1279001063ad7ccb9754db39c38b41fd
kofiad <kofiadaba@gmail.com>
2024-02-06 16:35:24 +0000

    task complete

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a Python data structure function"""
+import json
+
+
+def from_json_string(my_str):
+    """returns an object represented by a JSON string in Python
+
+    Args:
+        my_str (string): object
+
+    Returns:
+         Python data structure: converted JSON string to a Python data structure
+    """
+    return json.loads(my_str)
