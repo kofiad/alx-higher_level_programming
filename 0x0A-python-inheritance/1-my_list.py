64f6bf62c516edcf5245139781bcdffd32af6b57
kofiad <kofiadaba@gmail.com>
2024-02-07 18:28:24 +0000

    task complete

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a my_list class"""
+
+
+class MyList(List):
+    """ a class MyList that inherits from list
+
+    Args:
+        List (int): list
+    """
+    def __init__(self)
+        super().__init__()
+    def print_sorted(self):
+        """ prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
