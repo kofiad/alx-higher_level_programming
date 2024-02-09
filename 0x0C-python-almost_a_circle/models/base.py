fd0db41dae172fd6804ac0ac7c6c045f860458a9
Kofiad <kofiadaba@gmail.com>
2024-02-09 10:37:02 +0000

    work in progress

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a base class"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            base.__nb_objects += 1
+            self.id = base.__nb_objects
