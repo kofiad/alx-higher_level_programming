4e5fd0fa35329b2fae1ed05dce874e46b90d8205
kofiad <kofiadaba@gmail.com>
2024-02-12 19:44:52 +0000

    work in progress

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,3 +29,29 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file
+
+        Args:
+            list_objs (list): list of instances who inherits of Base
+        """
+        list_dicts = [obj.to_dictionary() for obj in list_objs] if list_objs else []
+        with open(cls.__name__ + '.json', 'w') as file:
+            file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string
+
+        Args:
+            json_string (string): a string representing a list of dictionaries
+
+        Returns:
+            list: JSON string representation
+        """
+        if not json_string:
+            return []
+        else:
+            return json.loads(json_string)
