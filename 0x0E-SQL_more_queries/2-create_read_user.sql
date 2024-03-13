caa41ce3709aea4f01dbf43650bf6adcf2963706
kofiad <kofiadaba@gmail.com>
2024-03-13 15:08:00 +0000

    task complete

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- user_0d_2 password should be set to user_0d_2_pwd
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY `user_0d_2_pwd`;
--- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
-GRANT SELECT ON hbtn_0d_2 .* TO user_0d_2@localhost;
+-- creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
