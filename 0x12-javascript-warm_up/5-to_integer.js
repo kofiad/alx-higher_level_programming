1243e59949fbdb43bf4fc95838e59b622de4b07a
Kofiad <kofiadaba@gmail.com>
2024-04-08 21:59:29 +0000

    work in progress

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('my number: ' + Number(process.argv[2]));
+  console.log('my number: ' + parseInt(process.argv[2]));
 }
