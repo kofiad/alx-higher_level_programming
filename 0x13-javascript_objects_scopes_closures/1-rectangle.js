c21bf45dbbab5478db45c6e010a8635e44b003cf
kofiad <kofiadaba@gmail.com>
2024-04-10 23:44:16 +0000

    work in progress

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * class Rectangle that defines a rectangle
+ * constructor takes 2 arguments w = width and h = height
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.wdth = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
