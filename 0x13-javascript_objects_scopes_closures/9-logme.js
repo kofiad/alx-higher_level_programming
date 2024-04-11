227f30d881c699c3a9ece5e26273f31341b7918f
kofiad <kofiadaba@gmail.com>
2024-04-11 13:16:55 +0000

    work in progress

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * a function that prints the number of arguments already printed and the new argument value.
+ * @param {string} item new argument value
+ */
+let count = 0;
+exports.logMe = function (item) {
+    console.log(`${count}: ${item}`);
+    count += 1;
+}
