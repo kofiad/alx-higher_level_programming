d750ac603b353806de02227d24fd8982b3c4b5fe
kofiad <kofiadaba@gmail.com>
2024-04-11 00:21:17 +0000

    work in progress

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -16,10 +16,10 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i <= this.height; i++) {
+    for (let i = 0; i < this.height; i++) {
       let row = '';
-      for (let j = 0; j <= this.width; j++) {
-        row += '#';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
       }
       console.log(row);
     }
